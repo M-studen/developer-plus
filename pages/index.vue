2bd4df53862e34475deed0c97ee196a94077946f
M-studen <zts090215@qq.com>
2023-08-12 21:49:33 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -15,10 +15,10 @@ function goToProject(link: string) {
 }
 
 onMounted(() => {
+      
+  alert("您的窗口过小，请打开桌面模式或放大窗口")
   if(window.screen.width<1226){
     console.log(window.screen.width);
-    
-    alert("您的窗口过小，请打开桌面模式或放大窗口")
   }
 })
 </script>
