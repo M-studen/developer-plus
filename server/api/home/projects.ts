1302e78bb65fce31bc0955b85ca6e0af289d8b49
M-studen <zts090215@qq.com>
2023-08-12 19:15:58 +0800

    Update projects.ts
    更新项目

--- server/api/home/projects.ts
+++ server/api/home/projects.ts
@@ -8,23 +8,8 @@ const withPrefix = (s: string) => `https://github.com/developer-plus/${s}`
 
 export default defineEventHandler<Project[]>(() => [
   {
-    name: 'developer-plus',
-    link: withPrefix('developer-plus'),
-    description: 'developer-plus 官网。'
-  },
-  {
-    name: 'amcss',
-    link: withPrefix('amcss'),
-    description: 'A minimalistic atomic css framework.'
-  },
-  {
-    name: 'mini-unocss',
-    link: withPrefix('mini-unocss'),
-    description: '一个即时按需的原子 CSS 引擎，unocss 的 mini 版本。'
-  },
-  {
-    name: 'vue-hbs-admin',
-    link: withPrefix('vue-hbs-admin'),
-    description: '为后台管理系统提供现成的开箱解决方案及丰富的示例，提高开发效率。'
+    name:"霸道乞丐爱上我",
+    link:"",
+    description:"一款galgame"
   }
 ])
