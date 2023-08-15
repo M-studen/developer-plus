8e6bbd4b7da58aa3389b31ecc7e71ae98776561f
M-studen <zts090215@qq.com>
2023-08-15 18:01:03 +0800

    Update projects.ts

--- server/api/home/projects.ts
+++ server/api/home/projects.ts
@@ -8,8 +8,8 @@ const withPrefix = (s: string) => `https://github.com/developer-plus/${s}`
 
 export default defineEventHandler<Project[]>(() => [
   {
-    name:"霸道乞丐爱上我",
-    link:"https://project.xks.xwsclub.top/2023/08/13/%E9%9C%B8%E9%81%93%E4%B9%9E%E4%B8%90%E7%88%B1%E4%B8%8A%E6%88%91/",
+    name:"我与病娇的日常生活",
+    link:"https://project.xks.xwsclub.top/2023/08/15/%E6%88%91%E5%92%8C%E7%97%85%E5%A8%87%E7%9A%84%E6%97%A5%E5%B8%B8%E7%94%9F%E6%B4%BB/",
     description:"一款galgame"
   }
 ])
